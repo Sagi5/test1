095694b63e40c7f0b8fcb23cc2b14e6673ada361
Sagi5 <nadarsagi@gmail.com>
2024-04-01 20:40:03 +0300

    dsf

---
+++ grades.py
@@ -0,0 +1,11 @@
+grades = [1,2,3,4,5]
+def calaverage(grades):
+    sum = 0
+    for i in range(len(grades)):
+        sum += grades[i]  
+    return(sum/len(grades))
+
+        
+
+
+
---
+++ main.py
@@ -0,0 +1,5 @@
+from grades import calaverage 
+from rich import print
+
+grades = [1,2,3,4,5]
+print(f"[italic blue]{calaverage(grades)}[/italic blue]")
